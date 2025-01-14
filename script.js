191f807bd3f3c31dcbf7b2511f114bc72339b05b
3Luan <nguyenthanhluanbntsbd@gmail.com>
2025-01-14 17:03:58 +0700

    update: progress ui

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function progressBar() {
+    let progress = document.getElementById("scrollPath");
+    let totalHeight = document.body.scrollHeight - window.innerHeight;
+    window.onscroll = function () {
+        let progressHeight = (window.scrollY / totalHeight) * 100;
+        progress.style.width = progressHeight + "%";
+    };
 }
